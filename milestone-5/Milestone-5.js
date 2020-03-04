8d338ce0a7b979165edbf4d40ddcb95fd4f8fd0d
Rachel Ouday <rachelami@gmail.com>
2020-03-04 20:50:13 +0200

    milestone-6

--- milestone-5/Milestone-5.js
+++ milestone-5/Milestone-5.js
@@ -1,26 +1,24 @@
-// function Fibonacci(x) {
-// 	let a = 0;
-// 	let b = 1;
-// 	let c = a + b;
-
-// 	for (i = 1; i < x; i++) {
-// 		c = a + b;
-// 		a = b;
-// 		b = c;
-// 	}
-
-// 	return c;
-// }
-
 function myFunction() {
-	let url = "http://localhost:5050/fibonacci/";
-	url = url + document.getElementById("myText").value;
-	fetch(url)
-		.then(response => response.json())
-		.then(data => {
-			console.log(data.number);
-			console.log(data.result);
-			console.log(data);
-			document.getElementById("y").innerText = data.result;
-		});
+	let useInput = document.getElementById("myText").value;
+	if (useInput > 50) {
+		document.getElementById("whenLoad").innerHTML = "ERROR!";
+	} else if (useInput == 42) {
+		document.getElementById("error").innerHTML =
+			"Server Error: 42 is the meaning of life";
+	} else {
+		const whenLoad = document.getElementById("whenLoad");
+		whenLoad.innerHTML = `
+	<div class="loader"></div>
+     `;
+		let url = "http://localhost:5050/fibonacci/";
+		url = url + useInput;
+		fetch(url)
+			.then(response => response.json())
+			.then(data => {
+				console.log(data.number);
+				console.log(data.result);
+				console.log(data);
+				document.getElementById("y").innerText = data.result;
+			});
+	}
 }
